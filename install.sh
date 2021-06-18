13c559a19e3ad6077db02b50bea73bc58bcb7075
Pycm <shoma81@users.noreply.github.com>
2021-06-18 23:51:14 +0300

    Updated install.sh
    Edited date of build

--- install.sh
+++ install.sh
@@ -117,7 +117,7 @@ print_modname() {
   ui_print ""
   ui_print "*   crDroid Android Patch      *"
   ui_print ""
-  ui_print "*     build 2021.05.09         *"
+  ui_print "*     build 2021.06.17         *"
   ui_print ""
   ui_print "********************************"
 }
